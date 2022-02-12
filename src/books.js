3eee40dd28181dcdc6df1baedfd7a98067ae4623
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-02-12 15:02:53 +0100

    commiting all files
    first commit

---
+++ src/books.js
@@ -0,0 +1,37 @@
+export const books = [
+  {
+    id: 1,
+    title: "Hiding in Plain Sight",
+    author: "Nuruddin Farah",
+    price: "$18.00",
+    image: "https://images3.penguinrandomhouse.com/cover/9781594634109",
+  },
+  {
+    id: 2,
+    title: "Behold the Dreamers",
+    author: "Imbolo Mbue",
+    price: "$17.00",
+    image: "https://images4.penguinrandomhouse.com/cover/9780525509714",
+  },
+  {
+    id: 3,
+    title: "The Thing Around Your Neck",
+    author: "Chimamanda Ngozi Adichie",
+    price: "$15.95",
+    image: "https://images2.penguinrandomhouse.com/cover/9780307455918",
+  },
+  {
+    id: 4,
+    title: "Allah is Not Obliged",
+    author: "Ahmadou Kourouma",
+    price: "$15.00",
+    image: "https://images2.penguinrandomhouse.com/cover/9780307279576",
+  },
+  {
+    id: 5,
+    title: "Snakepit",
+    author: "Moses Isegawa",
+    price: "$16.00",
+    image: "https://images1.penguinrandomhouse.com/cover/9780375719219",
+  },
+];
